593e518e401c0d7288abf79260d5ffe34bd5d3e8
Li Ge <geli19880206@gmail.com>
2012-10-31 12:20:03 -0700

    add gdb tests package

---
+++ TESTS/patternToJSON/.gdbinit
@@ -0,0 +1,6 @@
+set args main.tcl
+set logging on
+
+b jsonDomain.cpp:151
+
+run
---
+++ TESTS/patternToJSON/main.tcl
@@ -0,0 +1,5 @@
+cd /home/GL/Develop/server/node-opensees-server/files/shared/projects/truss;
+wipe all;
+source Truss.tcl;
+puts [json-echo-domain];
+puts [json-echo-disp];
